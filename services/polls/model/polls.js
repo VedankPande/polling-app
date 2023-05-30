8b127aae4d44ce1962c27c807f6743023c5b13c2
Vedank Pande <vedank.pande@gmail.com>
2023-05-31 00:50:20 +0530

    send document to MQ after save

--- services/polls/model/polls.js
+++ services/polls/model/polls.js
@@ -1,18 +1,31 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+import amqp from "amqplib/callback_api.js";
 
+import connectRabbitMQ from "../config/messaging.js";
+
+dotenv.config()
+
+const channel = await connectRabbitMQ(process.env.RABBIT_URL,process.env.RABBIT_QUEUE)
 
 const pollSchema = mongoose.Schema({
+  name: { type: String, index: { unique: false }, required: true },
+  owner: { type: String },
+  options: { type: [String] },
+  expire: { type: Date },
+});
 
-    name: {type: String, index: {unique: false},required:true},
-    owner: {type: String},
-    options: {type: [String]},
-    expire: {type: Date}
-})
 
-pollSchema.post('save', function(doc){
-    console.log("post save",doc)
-    //TODO: send message to votes here
-})
+// mongoose hook
+pollSchema.post("save", function (doc) {
+  try{
+    channel.sendToQueue(process.env.RABBIT_QUEUE, Buffer.from(JSON.stringify(doc)))
+    console.log(" [RabbitMQ] Sent %s", doc);
+  }
+  catch(error){
+    console.log(" [RabbitMQ]",error)
+  }
+  
+});
 
-export default pollSchema
-
+export default pollSchema;
