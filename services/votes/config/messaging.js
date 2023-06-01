49cd82cd472d721ca4523fef6ec7e918f123d032
Vedank Pande <vedank.pande@gmail.com>
2023-06-01 14:57:04 +0530

    rabbitMQ consumer

--- services/votes/config/messaging.js
+++ services/votes/config/messaging.js
@@ -1,7 +1,30 @@
 import amqp from "amqplib/callback_api.js";
+import mongoose from "mongoose";
 
 //TODO: Setup consumer for votes-poll queue here, with ack
 
-const messageConsumer = () =>{}
+const connectRabbitConsumer = (url, queue) => {
+    amqp.connect(url, (error, connection) => {
+        if (error) {
+          returnl
+        }
+        connection.createChannel((channelError,channel)=>{
+          if (channelError){
+              return;
+          }
+  
+          channel.assertQueue(queue,{
+              durable:false
+          })
+  
+          channel.consume(queue,(message)=>{
+            const messageJSON = JSON.parse(message.content)
+            console.log(`received message: ${JSON.stringify(messageJSON)}`)
+          },{noAck:true})
+  
+          console.log(`RabbitMQ Consumer listening for messages on ${queue}`)
+        })
+      });
+};
 
-export default messageConsumer
+export default connectRabbitConsumer;
