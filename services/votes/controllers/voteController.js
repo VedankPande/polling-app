57aaa5c6527a726c784676da0d6fd5e1111a489b
Vedank Pande <vedank.pande@gmail.com>
2023-06-01 14:58:10 +0530

    added dummy get method

--- services/votes/controllers/voteController.js
+++ services/votes/controllers/voteController.js
@@ -2,9 +2,22 @@ import mongoose from "mongoose";
 import votesSchema from "../model/votes.js";
 
 //TODO: Complete controllers
-const getVotes = (req,res)=>{
-    console.log(req.body)
-    res.json({})
+const getVotes = async (req,res)=>{
+
+    const {poll} = req.body
+
+    const Votes = mongoose.model('votes',votesSchema)
+
+    if (!poll){
+        res.json({
+            status: 400,
+            message: "Polls id is required"
+        })
+    }
+
+    const votes = await Votes.find({poll: poll}).exec()
+
+    res.json({votes})
 }
 
 const updateVotes = (req,res)=>{
