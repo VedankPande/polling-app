9fe129dfd1c88be8001c3259741ffce15776c146
Vedank Pande <vedank.pande@gmail.com>
2023-06-04 13:15:39 +0530

    get and patch method working with error handling

--- services/votes/controllers/voteController.js
+++ services/votes/controllers/voteController.js
@@ -5,24 +5,50 @@ import votesSchema from "../model/votes.js";
 const getVotes = async (req,res)=>{
 
     const {poll} = req.body
+    try{
+        const Votes = mongoose.model('votes',votesSchema)
 
-    const Votes = mongoose.model('votes',votesSchema)
-
-    if (!poll){
-        res.json({
-            status: 400,
-            message: "Polls id is required"
-        })
+        if (!poll){
+            res.json({
+                status: 400,
+                message: "Polls id is required"
+            })
+        }
+    
+        const votes = await Votes.find({poll: poll}).exec()
+        
+        console.log("GET votes complete")
+        res.json({status:200,message:votes})
+    }
+    catch(err){
+        console.log(err)
+        res.json({status:500,message:err})
     }
 
-    const votes = await Votes.find({poll: poll}).exec()
-
-    res.json({votes})
 }
 
-const updateVotes = (req,res)=>{
-    console.log(req.body)
-    res.json({})
+const updateVotes = async (req,res)=>{
+    
+    const {poll,vote} = req.body
+
+    if (!poll || !vote){
+        res.json({status:400,message:"Patch requires both id and vote"})
+    }
+    try{
+        const Votes = mongoose.model('votes',votesSchema)
+
+        await Votes.updateOne({poll: poll, "votes.option":vote},{$inc:{"votes.$.count":1}}).exec()
+    
+        const voteObject = await Votes.find({poll:poll}).exec()
+        
+        console.log("Updated votes object")
+        res.json({status:200,message:voteObject})
+    }
+    catch(err){
+        console.log(err)
+        res.json({status:500,message:err})
+    }
+
 }
 
 export {getVotes,updateVotes}
