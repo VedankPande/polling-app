ac56ccd16ffef53fd0370446990c9927b600b85d
Vedank Pande <vedank.pande@gmail.com>
2023-06-05 18:25:26 +0530

    create new file for message consumer handler methods

---
+++ services/votes/util/messageConsumerUtil.js
@@ -0,0 +1,29 @@
+
+const handleDelete = (message,model)=>{
+
+    //delete document
+    model.deleteOne({poll: message.id},{test:"Testvalue"}).then(()=>{
+        console.log(`vote document deleted for ${message.id}`)
+      }).catch((error)=>{
+        console.log(error)
+      })
+
+}
+
+const handleSave = (message,model)=>{
+    var votes = []
+                
+    //create a hashmap with votes and counts
+    for (const option of message.options){
+      votes.push({option,count:0})
+    }
+    
+    //save votes document
+    model.create({poll: message._id,votes}).then((doc)=>{
+        console.log(doc)
+    }).catch((error)=>{
+        console.log(error)
+    })
+}
+
+export { handleDelete,handleSave }
