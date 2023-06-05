b8dcf767c0f80e21b738426810f4c8988b3e7421
Vedank Pande <vedank.pande@gmail.com>
2023-06-05 18:24:22 +0530

    FIX: polls service id fix and update error handling for methods

--- services/polls/controllers/pollController.js
+++ services/polls/controllers/pollController.js
@@ -1,22 +1,40 @@
-import mongoose, { mongo } from "mongoose"; 
+import mongoose from "mongoose"; 
 import pollSchema from "../model/polls.js";
 
 const getPolls = async (req,res)=>{
     const {uuid} = req.body
     const Polls = mongoose.model('polls',pollSchema)
 
+    //validate request body
     if (!uuid){
         res.json({"status":400,
                   "message":"please provide a uuid"})
     }
 
-    const polls = await Polls.find({owner:uuid}).exec()
-    res.json({polls})
+    //Get poll and send response
+    Polls.find({owner:uuid}).exec().then((doc)=>{
+        console.log(doc)
+        res.json({status:200,message:doc})
+    }).catch((error)=>{
+        console.log(error)
+        res.json({status:500,message:`Error occurred when retrieving documents ${error}`})
+    })
 }
 
 const getPoll = (req,res)=>{
+    const poll = req.params.id
+    const Polls = mongoose.model('polls',pollSchema)
 
-    res.json({})
+
+    if (!poll){
+        res.json({status:400,message: "Poll is required"})
+    }
+
+    Polls.findOne({_id:poll}).exec().then((doc)=>{
+        res.json({status:200,message:doc})
+    }).catch((error)=>{
+        res.json({status:500,message:`Error occurred when trying to find Poll: ${error}`})
+    })
 
 }
 
@@ -26,22 +44,43 @@ const createPoll = async (req,res)=>{
     console.log(req.body)
     const {uuid,pollName,options,expire} = req.body 
 
-    try{
-        await new Polls({name:pollName,owner:uuid,options:options,expire:expire}).save()
+    //validate request body
+    if (!(uuid && pollName && options && expire)){
+        res.json({"status":400,
+                  "message":"all fields are required"})
+    }
 
-        console.log(`saved poll ${pollName}`)
-    
-    }
-    catch(err){
-        console.log(err)
-    }
-    
-    res.json({"status":200,"message":req.body})
+    //create poll and send response
+    Polls.create({name:pollName,owner:uuid,options:options,expire:expire}).then((doc)=>{
+        console.log(`Saved poll ${doc}`)
+        res.json({status:200,message:doc})
+    }).catch((error)=>{
+        console.log(error)
+        res.json({status:500,message:`Error occurred when trying to save Poll: ${error}`})
+    })
 }
 
+//TODO: Return poll that was deleted?
+//FIXME: Check if poll exists
 const deletePoll = (req,res)=>{
     console.log("delete poll request called")
-    res.json({})
+
+    //setup model
+    const Polls = mongoose.model('polls',pollSchema) 
+    const {poll} = req.body
+
+    //validate req body
+    if (!poll){
+        res.json({status:400,message:"Poll id is required"})
+    }
+
+    //Delete poll - passing poll id in options so that the post delete middleware can access it
+    Polls.deleteOne({_id:poll},{id: poll}).exec().then(()=>{
+        res.json({staus:200,message:"Deleted poll"})
+    }).catch((error)=>{
+        console.log(error)
+        res.json({status:500,message:`Error occurred while deleting document: ${error}`})
+    })
 }
 
 const updatePoll = (req,res)=>{
