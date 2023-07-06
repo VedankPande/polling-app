af9fcbeaf6c973ed49ab97238c0f5ba40c5036ad
Vedank Pande <vedank.pande@gmail.com>
2023-07-06 23:37:59 +0530

    added function to send queue message

--- services/polls/config/messaging.js
+++ services/polls/config/messaging.js
@@ -1,24 +1,27 @@
 //TODO: Create function that connects to RabbitMQ here and export
 import amqp from "amqplib/callback_api.js";
 
+//TODO: check if a new channel is created for every message send. Optimize if yes
 const connectRabbitMQ = (url, queue) => {
   return new Promise((resolve, reject) => {
     console.log("[RabbitMQ] Creating new connection")
     amqp.connect(url, (error, connection) => {
-      //deal with errors
+      //error creating connection
       if (error) {
         console.log("reject connection", error);
         reject(error);
         return;
       }
+      //error event in queue
       connection.on("error", function (err) {
         console.log("[AMQP]: Error:", err);
       });
+      //close event
       connection.on("close", () => {
         console.log("[AMQP]: Closed");
       });
 
-      //if successful connection
+      //successful connection
       connection.createChannel((channelError, channel) => {
         if (channelError) {
           console.log(channelError);
@@ -35,4 +38,17 @@ const connectRabbitMQ = (url, queue) => {
   });
 };
 
-export default connectRabbitMQ;
+const sendRabbitMessage = (channel, receiverQueue, message) =>{
+
+  try{
+    channel.sendToQueue(
+      receiverQueue,
+      Buffer.from(JSON.stringify(message))
+    )
+  } catch (error){
+    console.log(" [RabbitMQ]", error);
+  }
+
+}
+
+export {connectRabbitMQ,sendRabbitMessage};
