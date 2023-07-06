b8bb4281c6f51358fd863878730776ca6d11dece
Vedank Pande <vedank.pande@gmail.com>
2023-07-06 23:43:23 +0530

    send dummy messages to websockets server on hooks

--- services/votes/model/votes.js
+++ services/votes/model/votes.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+import connectRabbitMQ from "../messaging/myRabbitMQ.js"
+import sendRabbitMessage from "../messaging/rabbitProducer.js"
 dotenv.config()
+
+const channel = await connectRabbitMQ(process.env.RABBIT_URL,process.env.RABBIT_WEBSOCKET_QUEUE)
 
 const votesSchema = mongoose.Schema({
     poll: {type: String, index: { unique: true}, required: true},
@@ -11,12 +15,16 @@ const votesSchema = mongoose.Schema({
     }]
 })
 
-
+//TODO: NOT DRY - maybe create function to switch between hooks?
 votesSchema.post("save",(doc)=>{
+
+    sendRabbitMessage(channel,process.env.RABBIT_WEBSOCKET_QUEUE,{message:"saved vote"})
     console.log(doc)
 })
 
 votesSchema.post("deleteOne",function(doc){
+
+    sendRabbitMessage(channel,process.env.RABBIT_WEBSOCKET_QUEUE,{message:"saved vote"})
     console.log(doc)
 })
 
