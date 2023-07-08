2d82c9928379a6d9cb502aae74e712094965ebb8
Vedank Pande <vedank.pande@gmail.com>
2023-07-09 01:23:35 +0530

    add initial swagger schema

--- services/votes/model/votes.js
+++ services/votes/model/votes.js
@@ -3,6 +3,38 @@ import dotenv from "dotenv"
 
 import connectRabbitMQ from "../messaging/myRabbitMQ.js"
 import sendRabbitMessage from "../messaging/rabbitProducer.js"
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Votes:
+ *       type: object
+ *       required:
+ *         - poll
+ *         - votes
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the poll
+ *         poll:
+ *           type: string
+ *           description: The id of the poll that the votes correspond to
+ *         votes:
+ *           description: String to integer map that denotes votes per option
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               vote:
+ *                  type: integer
+ *                 
+ *       example:
+ *         id: d5fE_asz
+ *         poll: asdf14sedf
+ *         options: [{"A":0},{"B":1},{"C":3}]
+ */
+
 dotenv.config()
 
 const channel = await connectRabbitMQ(process.env.RABBIT_URL,process.env.RABBIT_WEBSOCKET_QUEUE)
