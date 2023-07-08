e8bd6889ac0b0e5082a8db0ceeed9b8b3ea7b8f6
Vedank Pande <vedank.pande@gmail.com>
2023-07-09 01:21:39 +0530

    add initial swagger schema

--- services/polls/model/polls.js
+++ services/polls/model/polls.js
@@ -3,6 +3,41 @@ import dotenv from "dotenv";
 
 import {connectRabbitMQ,sendRabbitMessage} from "../config/messaging.js";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Poll:
+ *       type: object
+ *       required:
+ *         - name
+ *         - owner
+ *         - options
+ *         - expire
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the poll
+ *         name:
+ *           type: string
+ *           description: The name of your poll
+ *         owner:
+ *           type: string
+ *           description: The id of the user that created the poll
+ *         options:
+ *           type: array
+ *           description: A list of vote options for the poll
+ *         exipire:
+ *           type: date
+ *           description: A timestamp that marks when the poll ends
+ *       example:
+ *         id: d5fE_asz
+ *         name: Election Poll
+ *         owner: asdfasdf_ASDf
+ *         options: [a,b,c,d]
+ *         expire: 2020-03-10T04:05:06.157Z
+ */
+
 dotenv.config();
 
 const channel = await connectRabbitMQ(
