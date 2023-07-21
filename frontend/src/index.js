a679f18c4be0abdbce5531518fbc3b8ae49fa1c4
Vedank Pande <vedank.pande@gmail.com>
2023-07-22 01:44:48 +0530

    React router setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/login";
+import Poll from "./components/poll";
+import Register from "./components/register";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AllPolls from "./components/userAllPolls";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path = "register" element={<Register />}/>
+      <Route path="poll" element={<Poll />} />
+      <Route path="home" element={<AllPolls />} />
+    </Routes>
   </BrowserRouter>
 );
 
