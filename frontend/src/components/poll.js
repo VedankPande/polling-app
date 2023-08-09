eeb9c03d492ed1c48c87ae42c4cea2b02f4965e2
Vedank Pande <vedank.pande@gmail.com>
2023-08-09 18:08:21 +0530

    fetch from api using router state

--- frontend/src/components/poll.js
+++ frontend/src/components/poll.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 import VoteContainer from "./votesContainer";
 import Vote from "./vote";
@@ -6,24 +8,40 @@ import "../styles/poll.css";
 
 export default function Poll() {
   const [responseData, setResponseData] = useState(null);
+  const {state} = useLocation()
 
-  const fetchData = () => {
+  const fetchData = async () => {
     //fetch data and set state here
+    axios
+      .get(`http://127.0.0.1:3080/votes?poll=${state.poll}`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        setResponseData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //TODO: Move ui to MUI completely 
   return (
     <div className="poll-container">
-      <div className="poll">
-        <h1>POLL TITLE</h1>
-        <VoteContainer>
-          <Vote />
-          <Vote />
-          <Vote />
-          <Vote />
-          <Vote />
-          <Vote />
-        </VoteContainer>
-      </div>
+      {responseData && (
+        <div className="poll">
+          <h1>{state.name}</h1>
+          <VoteContainer>
+            {responseData.message[0].votes.map((voteOption)=>
+              <Vote key={voteOption._id} data = {voteOption}/>
+            )}
+          </VoteContainer>
+        </div>
+      )}
     </div>
   );
 }
